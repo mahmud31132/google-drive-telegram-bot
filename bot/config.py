6453bf6052a908a350d0fc45a4c536962d47371c
Mahmud <73637283+mahmud31132@users.noreply.github.com>
2021-08-27 00:29:16 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -18,7 +18,7 @@ class BotCommands:
   Clone = ['copy', 'clone']
   Delete = ['delete', 'del']
   EmptyTrash = ['emptyTrash']
-  YtDl = ['ytdl']
+  YouTubeVideoDownload = ['utbe']
 
 class Messages:
     START_MSG = "**Hi there {}.**\n__I'm Google Drive Uploader Bot.You can use me to upload any file / video to Google Drive from direct link or Telegram Files.__\n__You can know more from /help.__"
